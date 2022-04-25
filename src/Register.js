ab3e4650c34043668292a30410fdf1d681ac5f3e
Gabriel Castro <gabriel.castro1028@gmail.com>
2022-04-25 15:50:10 -0700

    Register.js state

--- src/Register.js
+++ src/Register.js
@@ -8,7 +8,27 @@ const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 
 const Register = () => {
-  return (
+    //focus on field when first loads, also on error message
+    const useRef = useRef();
+    const errRef = errRef();
+    //user state, validate name, focus input field
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [ValidMatch,setValidMatch] = useState(false); 
+    const [matchFocus, setMatchFocus] = useState(false); 
+
+    const [ errMsg, setErrMsg] = useState('');
+    const [ success, setSuccess] = useState(false);
+
+
+       return (
     <div>
       
     </div>
